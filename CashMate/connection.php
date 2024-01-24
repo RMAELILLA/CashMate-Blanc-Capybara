45f5b73eb066c9d64c7b68b94417763655d884ee
Denise De Leon <129819435+ardy-dl@users.noreply.github.com>
2024-01-24 15:05:51 +0800

    Update connection.php

--- CashMate/connection.php
+++ CashMate/connection.php
@@ -2,7 +2,5 @@
 $con = new mysqli('127.0.0.1', 'cashmate', 'BlancCapybara', 'login_cashmate_db');
 if ($con->connect_error) {
     die('Failed to connect to MySQL: ' . $con->connect_error);
-} else {
-    echo 'Connected successfully!';
 }
 ?>
